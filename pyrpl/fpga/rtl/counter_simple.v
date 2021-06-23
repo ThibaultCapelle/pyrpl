3259bdf3461de21d6ec16960212d2c02a2cbdaf3
ThibaultCapelle <thibault.capelle@nbi.ku.dk>
2021-06-23 10:25:55 +0200

    another bug fix on counter_simple

--- pyrpl/fpga/rtl/counter_simple.v
+++ pyrpl/fpga/rtl/counter_simple.v
@@ -6,21 +6,22 @@ module counter_simple (
 	output reg [1:0] overflow
 );
 
-reg [31:0] timer_reg, timer_next;
-reg counting
+	reg [31:0] timer_reg, timer_next;
+	reg counting
 
-always @ (posedge clk)
-	timer_reg<=timer_next;
+	always @ (posedge clk)
+		timer_reg<=timer_next;
 
-always @*
-	if (start)
-		timer_next=N;
-	else if ((timer_tick) && (timer_reg != 0)) begin
-		timer_next=timer_reg-1;
-		counting=1;
-		end
-	else begin
-		timer_next=timer_reg;
-		counting=0;
-		end
-assign overflow = {timer_reg==0,counting==1};
+	always @ (*)
+		if (start)
+			timer_next=N;
+		else if ((timer_tick) && (timer_reg != 0)) begin
+			timer_next=timer_reg-1;
+			counting=1;
+			end
+		else begin
+			timer_next=timer_reg;
+			counting=0;
+			end
+	assign overflow = {timer_reg==0,counting==1};
+endmodule
